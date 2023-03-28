208ab63d1c728570d5342189be29f3350ec05baa
Cesar <c.augusto_mendez@hotmail.com>
2023-03-27 23:26:01 -0400

    Si se ingresa 1 retonra 1

--- src/sumar_cadenas.js
+++ src/sumar_cadenas.js
@@ -1,14 +1,7 @@
 function sumar_cadenas(cadena) {
   let suma = 0;
-  if(cadena.length > 1){
-    let num1 = + cadena[0];
-    let num2 = +cadena[2];
-    suma = num1 + num2;
-  }
-  else{
     suma += +cadena;
-}
-return suma;
+    return suma;
 }
 
 export default sumar_cadenas;
