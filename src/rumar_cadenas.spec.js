8e9b2844b1bd920be33bcdce6e7253462e0c6dd2
Cesar <c.augusto_mendez@hotmail.com>
2023-03-27 23:28:04 -0400

    Si se ingresa una cadena de 2 o mas valores, retorna la suma de esta

--- src/rumar_cadenas.spec.js
+++ src/rumar_cadenas.spec.js
@@ -10,4 +10,7 @@ describe("Sumar", () => {
   it("deberia devolver la suma de 1 y 2", () => {
     expect(sumar_cadenas("1,2")).toEqual(3);
   });
+  it("deberia devolver la suma de 2 o mas valores", () => {
+    expect(sumar_cadenas("1,2,3,4,5")).toEqual(15);
+  });
 });
