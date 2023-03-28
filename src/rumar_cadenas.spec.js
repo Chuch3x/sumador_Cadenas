21a2faa7c498218d8ad326d2f0f3ae02318d7a06
Cesar <c.augusto_mendez@hotmail.com>
2023-03-27 23:25:00 -0400

    Si se ingresa una cadena vacia se retorna 0

---
+++ src/rumar_cadenas.spec.js
@@ -0,0 +1,13 @@
+import sumar_cadenas from "./sumar_cadenas";
+
+describe("Sumar", () => {
+  it("deberia devolver el valor 0", () => {
+    expect(sumar_cadenas("")).toEqual(0);
+  });
+  it("deberia devolver la suma de 1", () => {
+    expect(sumar_cadenas("1")).toEqual(1);
+  });
+  it("deberia devolver la suma de 1 y 2", () => {
+    expect(sumar_cadenas("1,2")).toEqual(3);
+  });
+});
---
+++ src/sumar_cadenas.js
@@ -0,0 +1,14 @@
+function sumar_cadenas(cadena) {
+  let suma = 0;
+  if(cadena.length > 1){
+    let num1 = + cadena[0];
+    let num2 = +cadena[2];
+    suma = num1 + num2;
+  }
+  else{
+    suma += +cadena;
+}
+return suma;
+}
+
+export default sumar_cadenas;
